c87c34a1bf8081113ea3b714096bb89b068b1fc2
Hamad Fuad <ihamadfouad@icloud.com>
2024-04-24 13:07:08 +0300

    Add binary target

--- Package.swift
+++ Package.swift
@@ -10,13 +10,17 @@ let package = Package(
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "TasheelSDKSwift",
-      targets: ["TasheelSDKSwift"]),
+      targets: ["TasheelSDKSwift", "TasheelSDK"]),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "TasheelSDKSwift"),
+    .binaryTarget(
+      name: "TasheelSDK",
+      path: "Sources/TasheelSDKSwift/TasheelSDK.xcframework"
+    ),
     .testTarget(
       name: "TasheelSDKSwiftTests",
       dependencies: ["TasheelSDKSwift"]),
