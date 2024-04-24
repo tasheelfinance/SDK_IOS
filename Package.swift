b60727c41986905f9c1a6b6092b242405c74eb45
Hamad Fuad <ihamadfouad@icloud.com>
2024-04-24 12:59:29 +0300

    Init commit

--- Package.swift
+++ Package.swift
@@ -4,20 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "TasheelSDKSwift",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "TasheelSDKSwift",
-            targets: ["TasheelSDKSwift"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TasheelSDKSwift"),
-        .testTarget(
-            name: "TasheelSDKSwiftTests",
-            dependencies: ["TasheelSDKSwift"]),
-    ]
+  name: "TasheelSDKSwift",
+  platforms: [.iOS(.v15)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "TasheelSDKSwift",
+      targets: ["TasheelSDKSwift"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "TasheelSDKSwift"),
+    .testTarget(
+      name: "TasheelSDKSwiftTests",
+      dependencies: ["TasheelSDKSwift"]),
+  ]
 )
---
+++ Sources/TasheelSDKSwift/TasheelSDK.xcframework/ios-arm64_x86_64-simulator/TasheelSDK.framework/Headers/TasheelSDK.h
@@ -0,0 +1,18 @@
+//
+//  TasheelSDK.h
+//  TasheelSDK
+//
+//  Created by Hamad Fuad on 14/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TasheelSDK.
+FOUNDATION_EXPORT double TasheelSDKVersionNumber;
+
+//! Project version string for TasheelSDK.
+FOUNDATION_EXPORT const unsigned char TasheelSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TasheelSDK/PublicHeader.h>
+
+
